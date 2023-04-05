99b368d10f00f0808197d6081fc2792c5fdcf617
Pavel_Yudakov <pavels.judakovs@inbox.lv>
2023-04-05 16:46:28 +0300

    Update 05.04.23

---
+++ src/main/java/lesson3/weekdays/CalendarTest.java
@@ -0,0 +1,36 @@
+package lesson3.weekdays;
+
+import java.util.Scanner;
+
+public class CalendarTest {
+
+    public static void main(String[] args) {
+
+        Week week = new Week(WeekDays.SUNDAY);
+
+        System.out.println(week.Days(WeekDays.MONDAY));
+        System.out.println("-------------------------------- ");
+
+//        System.out.println(week.isWeekEnd(WeekDays.SUNDAY));
+  //      System.out.println(week.isWeekEnd(WeekDays.MONDAY));
+  //      System.out.println(week.isWorkingDay(WeekDays.SUNDAY));
+
+//        System.out.println(MONDAY);
+//        System.out.println(SUNDAY.ordinal()); // порядковый номер элемента
+//        System.out.println(
+//                Arrays.toString(
+//                        WeekDays.values()
+//                )
+//            );
+
+//        WeekDays day = WeekDays.FRIDAY;
+//        WeekDays day1 = WeekDays.valueOf("WEDNESDAY");
+//        System.out.println((WeekDays.valueOf("WEDNESDAY").ordinal()));
+//        System.out.println(WeekDays.values()[2]);
+//    }
+//
+//
+    }
+
+}
+
--- src/main/java/lesson3/weekdays/WeekDays.java
+++ src/main/java/lesson3/weekdays/WeekDays.java
@@ -9,35 +9,5 @@ public enum WeekDays {
     THURSDAY,
     FRIDAY,
     SATURDAY,
-    SUNDAY;
-
-    public boolean isWorkDay()
-    {
-        // возвращать true если день с понедельника по пятницу включительно
-        return true;
-    }
-
-    public boolean isWeekEnd()
-    {
-        // возвращать true если день суббота или воскресенье
-        return true;
-    }
-
-
-    public static void main(String[] args) {
-        System.out.println(MONDAY);
-        System.out.println(SUNDAY.ordinal()); // порядковый номер элемента
-        System.out.println(
-                Arrays.toString(
-                        WeekDays.values()
-                )
-            );
-
-        WeekDays day = WeekDays.FRIDAY;
-        WeekDays day1 = WeekDays.valueOf("WEDNESDAY");
-        System.out.println((WeekDays.valueOf("WEDNESDAY").ordinal()));
-        System.out.println(WeekDays.values()[2]);
-    }
-
-
+    SUNDAY
 }
